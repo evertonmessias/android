7d661284dca9223779b6cf9b6ef6ff92f1264881
evertonmessias <everton.messias@gmail.com>
2018-03-26 16:17:18 -0300

    26-03-2018_16:17:18

---
+++ Forca/app/src/main/java/everton/m/forca/SplashActivity.java
@@ -0,0 +1,26 @@
+package everton.m.forca;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
+
+
