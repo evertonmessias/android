d130c95429eeaf19a79e45bb5d22de76b41dc74c
evertonmessias <everton.messias@gmail.com>
2018-08-18 20:28:49 -0300

    18-08-2018_20:28:45

---
+++ Eq2grau/app/src/main/java/evertonm/eq2grau/MainActivity.java
@@ -0,0 +1,96 @@
+package evertonm.eq2grau;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.String.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    float valorA, valorB, valorC, d, x1, x2, vc[] = new float[4];
+    int op;char q = (char) 178;
+
+    String escrever() {
+        String eq, a, b, c;
+        a = format("%.1f", valorA);b = format("%.1f", valorB);c = format("%.1f", valorC);
+        if (valorB < 0 && valorC < 0) {
+            eq = a + "x" + q + " " + b + "x " + c + " = 0";
+        } else if (valorB >= 0 && valorC < 0) {
+            eq = a + "x" + q + " +" + b + "x " + c + " = 0";
+        } else if (valorB < 0 && valorC >= 0) {
+            eq = a + "x" + q + " " + b + "x +" + c + " = 0";
+        } else {eq = a + "x" + q + " +" + b + "x +" + c + " = 0";}
+        return eq;}
+
+    float[] calcular() {// [0] delta ; [1] x1 ; [2] x2 ; [3] resposta
+        if (valorA == 0) {vc[0] = 0;vc[1] = 0;vc[2] = 0;vc[3] = 0;}//"A NãoZero!"
+        else {d = (float) ((pow(valorB, 2)) - (4 * valorA * valorC)); // cálculo do delta
+            if (d < 0) {vc[0] = d;vc[1] = 0;vc[2] = 0;vc[3] = 1;}//"Não Existe Raizes!"
+            else {// cálculo de Basckara
+                x1 = (float) (((-1 * valorB) + (sqrt(d))) / (2 * valorA));
+                x2 = (float) (((-1 * valorB) - (sqrt(d))) / (2 * valorA));
+                vc[0] = d;vc[1] = x1;vc[2] = x2;vc[3] = 2;}} //ok
+        return vc;}
+
+    String resposta(int op) {switch (op) {
+            case 0:return "Valor 'A' não pode ser Zero!";
+            case 1:return "Não Existem Raizes Reais!";
+            default:return escrever();}}
+
+    public void escondeTeclado(){
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+
+            // ------------------------------BOTÕES-----------------------------------------
+
+    public void clique1(View view) {
+        escondeTeclado();
+        // tipo variavel = metodo de captura
+        EditText va = findViewById(R.id.va);
+        EditText vb = findViewById(R.id.vb);
+        EditText vc = findViewById(R.id.vc);
+        TextView resposta = findViewById(R.id.resposta);
+        TextView valores = findViewById(R.id.valores);
+
+        if (va.getText().length() > 0 && vb.getText().length() > 0 && vc.getText().length() > 0) {
+
+            String a = va.getText().toString();
+            String b = vb.getText().toString();
+            String c = vc.getText().toString();
+
+            valorA = Float.valueOf(a);valorB = Float.valueOf(b);valorC = Float.valueOf(c);
+
+            op = (int) calcular()[3];resposta.setText(resposta(op));
+            String de = format("%.1f", calcular()[0]);
+            String r1 = format("%.1f", calcular()[1]);
+            String r2 = format("%.1f", calcular()[2]);
+            valores.setText("delta = " + de +"  |  x1 = " + r1 + "  |  x2 = " + r2);
+        } else {resposta.setText("Digite os Valores!");}}
+
+    public void clique2(View view) {
+        escondeTeclado();
+        onRestart();
+        EditText va = findViewById(R.id.va);
+        EditText vb = findViewById(R.id.vb);
+        EditText vc = findViewById(R.id.vc);
+        TextView resposta = findViewById(R.id.resposta);
+        TextView valores = findViewById(R.id.valores);
+
+        va.setText("");vb.setText("");vc.setText("");
+        resposta.setText("Resposta:");
+        valores.setText("delta = 0  |  x1 = 0  |  x2 = 0");}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);}}
